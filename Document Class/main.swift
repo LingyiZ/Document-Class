a9478ec1faaff3cd2f330004eb6aa885a9338bff
Lingyi Zhan <shinycatherine01@gmail.com>
2020-12-20 19:45:44 -0600

    Update main.swift

--- Document Class/main.swift
+++ Document Class/main.swift
@@ -8,5 +8,35 @@
 
 import Foundation
 
-print("Hello, World!")
+//define class
+class Document {
+    var title: String
+    var body = ""
+    //getter to return computed property length
+    var length: Int {
+        get {
+            return body.count
+        }
+    }
+    
+    //initialization
+    init(title: String) {
+        self.title = title
+    }
+    
+    init(title: String, body: String) {
+        self.title = title
+        self.body = body
+    }
+    
+}
 
+let document1 = Document(title: "Hello World")
+document1.title = "Hello World!"
+document1.body = "I awoke from a digital slumber."
+let document2 = Document(title: "The Day", body: "It was a day to remember.")
+print(document1.title)
+print(document1.body)
+print(document1.length)
+print(document2.title)
+print(document2.length)
